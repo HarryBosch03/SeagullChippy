00e55550ac6c76fbc8986eb70130066a0b9ac263
HarryBosch03 <harrybosch03@gmail.com>
2023-06-24 18:37:25 +1000

    [UNSTABLE] Backup

--- Assets/Code/Scripts/Session/GameSession.cs
+++ Assets/Code/Scripts/Session/GameSession.cs
@@ -1,3 +1,4 @@
+using ShootingRangeGame.AI.BehaviourTrees.Core;
 using ShootingRangeGame.Saves;
 using TMPro;
 using UnityEngine;
@@ -19,26 +20,25 @@ namespace ShootingRangeGame.Session
         [Header("Audio")]
         [SerializeField] private AudioSource roundStart;
         [SerializeField] private AudioSource roundEnd;
-        
+
         private int highScore;
         private bool roundActive;
         
-        public void OnAwake()
-        {
-            roundStart = GetComponent<AudioSource>();
-            roundEnd = GetComponent<AudioSource>();
-        }
-
         public int Score { get; private set; }
+        public float RoundTimer { get; private set; }
+        public static GameSession Active { get; private set; }
         public static int HighScore
         {
             get => SaveManager.GetOrLoad().highScore;
             set => SaveManager.GetOrLoad().highScore = value;
         }
         
-        public float RoundTimer { get; private set; }
-        public static GameSession Active { get; private set; }
-
+        public void OnAwake()
+        {
+            roundStart = GetComponent<AudioSource>();
+            roundEnd = GetComponent<AudioSource>();
+        }
+        
         private void Start()
         {
             RefreshUI();
